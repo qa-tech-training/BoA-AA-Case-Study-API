a164563530fed848799387b5a3fd88334a5cbe50
agray <agray@qa.com>
2025-10-21 08:24:34 +0100

    prettify store.py

--- api/store.py
+++ api/store.py
@@ -1,2 +1,6 @@
 from random import randint
-store = {"sandboxes": [], "operations": [], "ips": [".".join(str(randint(0,255)) for i in range(4)) for i in range(100)]}
+store = {
+    "sandboxes": [], 
+    "operations": [], 
+    "ips": [".".join(str(randint(0,255)) for i in range(4)) for i in range(100)]
+}
